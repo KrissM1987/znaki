866de5655a880ea29f30ba9433726846bcde5dde
KrissM1987 <krissm@poczta.fm>
2018-10-11 17:34:30 +0200

    znaki

---
+++ znaki.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+
+int main  ()
+{
+ puts("Pierwszy progs");
+
+putchar('g');
+//printf ("blablabla \t");
+printf ("       sasasasasasa\n" );
+//putchar ('\r');
+printf ("dsdsdsdsds ");
+putchar ('\n'); // znak końca linii
+putchar ('\t'); // tabulator
+/*   putchar ('\r'); */ /*powrot karetki- kursor wraca na poczatek linii*/
+putchar('\\'); //wypisuje \ 
+return 0;
+}
